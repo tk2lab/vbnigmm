70897dfb2f9e8648049bdfd8eac3e9546de7f49a
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-09-20 10:22:35 +0900

    Add Normal distribution

--- src/vbnigmm/gauss_posterior.py
+++ src/vbnigmm/gauss_posterior.py
@@ -5,7 +5,7 @@ __credits__ = 'Copyright 2020, TAKEKAWA Takashi'
 import numpy as np
 import scipy.special as sp
 
-from .distributions import BaseDist, Dirichlet, Wishart
+from .distributions import BaseDist, Dirichlet, Normal, Wishart
 
 
 class BayesianGaussianMixturePosterior(BaseDist):
@@ -13,7 +13,7 @@ class BayesianGaussianMixturePosterior(BaseDist):
     def __init__(self, l, r, s, t, u, m):
         alpha = Dirichlet(l, r)
         tau = Wishart(s, t, inv=True)
-        mu = _Mean(tau, u, m)
+        mu = Normal(tau, u, m)
         self.params = l, r, s, t, u, m
         self._dists = alpha, tau, mu
 
@@ -58,31 +58,3 @@ class BayesianGaussianMixturePosterior(BaseDist):
             + tau.log_det_mean / 2
             - mu.mahalanobis(x) / 2
         )
-
-
-class _Mean(BaseDist):
-
-    def __init__(self, tau, *args):
-        u, m = map(np.asarray, args)
-        self.tau = tau
-        self.params = u, m
-
-    def cross_entropy(self, *args):
-        u, m = map(np.asarray, args)
-        u1, m1 = self.params
-        d = m.shape[-1]
-        x = m1 - m
-        xtau = np.sum(x[..., None, :] * self.tau.mean, axis=-1)
-        return - (
-            - sp.xlogy(d / 2, 2 * np.pi)
-            + sp.xlogy(d / 2, u)
-            + self.tau.log_det_mean / 2
-            - u * (d / u1 + np.sum(xtau * x, axis=-1)) / 2
-        )
-
-    def mahalanobis(self, x):
-        u, m = self.params
-        d = m.shape[-1]
-        x = x[..., None, :] - m
-        xtau = np.sum(x[..., None, :] * self.tau.mean, axis=-1)
-        return d / u + np.sum(xtau * x, axis=-1)
